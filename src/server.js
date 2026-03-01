0d245e6aa2ca6ea206b92c481a7925062137b44b
dmohamed24 <dahir.mohamedd24@gmail.com>
2026-03-01 00:27:43 +0000

    SHAM-SERVER-3: Created a basic dynamic server without any validation using config file

--- src/server.js
+++ src/server.js
@@ -3,12 +3,23 @@ import express from "express";
 const createServer = (config) => {
   const app = express();
 
-  console.log(config);
+  const routesArr = config.routes;
+
+  routesArr.forEach(({ method, path, status, delay, response }) => {
+    app[method.toLowerCase()](path, (req, res) => {
+      try {
+        setTimeout(() => {
+          return res.status(status).json(response);
+        }, delay);
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+  });
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
-
   return app;
 };
 
